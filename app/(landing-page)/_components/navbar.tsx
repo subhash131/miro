6696d4148fc10e5113d2c9bbcbc1de9b90786061
subhash131 <subhashnayak131@gmail.com>
2024-06-08 18:53:41 +0530

    add landing page

--- app/(landing-page)/_components/navbar.tsx
+++ app/(landing-page)/_components/navbar.tsx
@@ -1,7 +1,39 @@
+import { Button } from "@/components/ui/button";
+import { SignInButton, SignUpButton } from "@clerk/nextjs";
+import Image from "next/image";
 import React from "react";
 
 const Navbar = () => {
-  return <div className="fixed top-0 w-screen h-20 bg-white">Navbar</div>;
+  return (
+    <div className="fixed top-0 w-screen h-20 bg-white flex items-center justify-between px-10">
+      <div className="flex gap-2 items-center">
+        <Image
+          src="/logo.png"
+          alt="logo"
+          width={10}
+          height={10}
+          className="size-10"
+        />
+        <h1 className="text-3xl font-semibold">miro</h1>
+      </div>
+      <div className="flex gap-4">
+        <Button
+          asChild
+          variant="outline"
+          className="border-black hover:bg-black hover:text-white transition"
+        >
+          <SignInButton>Login</SignInButton>
+        </Button>
+        <Button
+          asChild
+          variant="secondary"
+          className="bg-blue-600 text-white hover:bg-blue-900 text-base"
+        >
+          <SignUpButton>Sing up free</SignUpButton>
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
--- app/(landing-page)/page.tsx
+++ app/(landing-page)/page.tsx
@@ -4,6 +4,7 @@ import { Button } from "../../components/ui/button";
 import { Unauthenticated } from "convex/react";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
+import Navbar from "./_components/navbar";
 
 export default function Home() {
   const { isSignedIn } = useSession();
@@ -14,9 +15,28 @@ export default function Home() {
   }, [isSignedIn, router]);
   return (
     <Unauthenticated>
-      <SignInButton>
-        <Button>Sign in</Button>
-      </SignInButton>
+      <Navbar />
+      <div className="w-screen h-screen bg-[#FCF7EF] pt-20 flex items-center justify-center flex-col">
+        <div className="flex flex-col items-center gap-10">
+          <div className="text-6xl flex flex-col gap-1 text-center">
+            <p>Think and create. </p>
+            <p>All in one visual workspace.</p>
+          </div>
+          <div className="flex flex-col gap-1 text-center text-base items-center justify-center">
+            <p>Go from brainstorming to execution, all in one place.</p>
+            <p>Miro is your team&apos;s collaborative online workspace.</p>
+          </div>
+          <Button
+            asChild
+            className="bg-blue-600 text-white hover:bg-blue-900 w-96 py-6 text-lg"
+          >
+            <SignInButton>Sing up free</SignInButton>
+          </Button>
+        </div>
+        <p className="text-xs text-neutral-500 mt-8">
+          Keep work and life separate by using your work email
+        </p>
+      </div>
     </Unauthenticated>
   );
 }
